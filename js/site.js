aed1e23b8cca9966849312decbeae4421cf32d7c
Vítor Avelino <vitoravelino@octahedron.com.br>
2010-09-28 17:32:38 -0300

    added random header stuff

---
+++ js/site.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+	$("header#top").css("background-image", "url(file:///home/vitoravelino/Development/octahedron/site/images/header" + (Math.floor(Math.random() * 13 - 0.0001) + 1) + ".png)");
+
+});
+
