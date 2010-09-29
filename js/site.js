668501752654288c3049e6747b707833b0b6398e
Vítor Avelino <vitoravelino@octahedron.com.br>
2010-09-28 22:00:14 -0300

    reduced images sizes

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$("header#top").css("background-image", "url(http://www.octahedron.com.br/images/header" + (Math.floor(Math.random() * 13 - 0.0001) + 1) + ".png)");
+	$("header#top").css("background-image", "url(http://www.octahedron.com.br/images/header" + (Math.floor(Math.random() * 13 - 0.0001) + 1) + ".jpg)");
 
 });
 
