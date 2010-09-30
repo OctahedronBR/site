54444cfe2ba0074d2ec62151310bb177bcf22f95
Vítor Avelino <vitoravelino@octahedron.com.br>
2010-09-30 19:39:23 -0300

    minified scroll.js

--- js/site.js
+++ js/site.js
@@ -2,48 +2,5 @@ $(document).ready(function() {
 
 	$("header#top").css("background-image", "url(http://www.octahedron.com.br/images/header" + (Math.floor(Math.random() * 13 - 0.0001) + 1) + ".jpg)");
 
-	/**
-	 * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
-	 * @see http://flesler.demos.com/jquery/scrollTo/
-	 * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.LocalScroll.
-	 */
-
-	// The default axis is 'y', but in this demo, I want to scroll both
-	// You can modify any default like this
-	$.localScroll.defaults.axis = 'x';
-
-	// Scroll initially if there's a hash (#something) in the url
-	$.localScroll.hash({
-		target: '#multiple', // Could be a selector or a jQuery object too.
-		queue: true,
-		duration: 1500,
-		onAfter:function( anchor, settings ){
-			if (anchor.id == 'equipe') {
-				$('article#multiple').animate({height: "593px"}, 1000 );
-			} else {
-				$('article#multiple').animate({height: "255px"}, 1000 );
-			}
-		}
-
-	});
-
-	/**
-	 * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-	 * also affect the >> and << links. I want every link in the page to scroll.
-	 */
-	$.localScroll({
-		target: '#multiple', // could be a selector or a jQuery object too.
-		queue:true,
-		duration:1000,
-		hash:true,
-		onAfter:function( anchor, settings ){
-			if (anchor.id == 'equipe') {
-				$('article#multiple').animate({height: "593px"}, 1000 );
-			} else {
-				$('article#multiple').animate({height: "255px"}, 1000 );
-			}
-		}
-
-	});
 });
 
