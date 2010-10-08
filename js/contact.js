ff476e41a413c5b425d922bd66cdf91d5a3d197f
Vítor Avelino <vitoravelino@octahedron.com.br>
2010-10-07 21:33:07 -0300

    added more content and links

--- js/contact.js
+++ js/contact.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
 		$.ajax({
 		  type: "POST",
 		  url: "http://email.latest.octahedronpage.appspot.com/email",
-		  data: "name=vitor&email=teste@teste.com&message=mensagem",
+		  data: "name=vitor&email=teste@teste.com&message=mensagem&jsoncallback=?",
 		  success: function(data) {
-		  	alert(data);
+		  	console.log(data);
 		  }
+		  dataType: "jsonp",
+		  jsonp: '_jsonp'
 		});
 
 		return false;
