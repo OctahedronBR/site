a284634698f8012f90854342e61135c66556a0a2
Vítor Avelino <vitoravelino@octahedron.com.br>
2010-10-13 17:36:32 -0300

    added contact ajax function

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
 	$('#submit').click(function() {
-		$.ajax({
-		  type: "POST",
-		  url: "http://email.latest.octahedronpage.appspot.com/email",
-		  data: "name=vitor&email=teste@teste.com&message=mensagem&jsoncallback=?",
-		  success: function(data) {
-		  	console.log(data);
-		  }
-		  dataType: "jsonp",
-		  jsonp: '_jsonp'
-		});
+		var name = $("input#name").val();
+		var email = $("input#email").val();
+		var message =  $("textarea").val();
+
+		if (name.length != 0 && email.length != 0 && message.length != 0) {
+			$.ajax({
+				type: "POST",
+				url: "http://www.octahedron.com.br/email",
+				data: "name=" + name + "&email=" + email + "&message=" + message + "&callback=?",
+				dataType: "json",
+				success: function(data) {
+					if (data.status == "ok") {
+						$("p.warning").html("Contato feito com sucesso!");
+						$("textarea").empty();
+					} else {
+						$("p.warning").html("Preencha o formulário corretamente!");
+					}
+				},
+				error: function () {
+					$("p.warning").html("Problema com o servidor de email! Por favor, entre em contato manualmente via email.");
+				}
+			});
+		} else {
+			$("p.warning").html("Preencha o formulário corretamente!");
+		}
 
 		return false;
 	});
