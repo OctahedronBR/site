2aef73497ff964f6ac1c7c3ed7da4a9ae5d8bbb2
Danilo Penna Queiroz <daniloqueiroz@octahedron.com.br>
2010-10-16 16:18:00 -0300

    new index moved to index, old index moved to construction.html. some spell checks at contato and empresa pages. some quotes added to site.js

--- js/site.js
+++ js/site.js
@@ -2,15 +2,11 @@ $(document).ready(function() {
 	$("header#top").css("background-image", "url(http://www.octahedron.com.br/images/header" + (Math.floor(Math.random() * 13 - 0.0001) + 1) + ".jpg)");
 
 	var quotes = new Array(
-		"O tempo é sempre certo para fazer o que está certo. (Martin Luther King, Jr.)",
-		"Obstáculos são as coisas assustadoras que encontra quando desvia os olhos do seu sonho. (Henry Ford)",
+		"O melhor jeito de prever o futuro é inventá-lo. (Alan Kay)",
+		"Estamos apostando todas as fichas na nuvem.(Steve Ballmer)" 
 		"Para começar, pare de falar e comece a fazer. (Walt Disney)",
 		"Você torna-se aquilo em que pensa. (Earl Nightingale)",
-		"Se queres ser feliz amanhã, tenta hoje mesmo. (Liang Tzu)",
-		"A vitória pertence ao mais perseverante. (Napoleão Bonaparte)",
 		"Eu descobri que sempre tenho escolhas. E muitas vezes, trata-se apenas de uma escolha de atitude. (Judith M. Knowlton)",
-		"Fazemos nossos caminhos e lhes chamamos destino. (Benjamin Disraeli)",
-		"Grandes realizações não são feitas por impulso, mas por uma soma de pequenas realizações. (Vincent Van Gogh)"
 	);
 
 	if ($("p#quote").length != 0) {
--- spell_check.sh
+++ spell_check.sh
@@ -2,13 +2,13 @@
 
 if [ ! -f /usr/bin/aspell ]
 then
-	echo "you need to install aspell"
+	echo "you need to install aspell: sudo apt-get install aspell aspell-pt-br"
 	exit 42
 else 
 	$(aspell dump dicts | grep pt_BR > /dev/null)
 	if [ $? -ne 0 ]
 	then
-		echo "you need install pt_BR dict for aspell"
+		echo "you need install pt_BR dict for aspell: sudo apt-get install aspell-pt-br"
 		exit 42
 	fi
 fi
