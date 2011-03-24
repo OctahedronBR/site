913c8f64207efa638b840cd9ff792541df2e1eac
Vítor Avelino <vitoravelino@octahedron.com.br>
2011-03-24 13:52:07 -0300

    presentation source separated and social index refactored

--- js/social.js
+++ js/social.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 	$.getJSON(
-		'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=octahedronbr&include_rts=true&count=5&callback=?',
+		'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=octahedronbr&include_rts=true&count=3&callback=?',
 		function(data) {
+			var img;
 			$("section#twitter ul").hide();
 			for (index in data) {
-				$("section#twitter ul").append("<li>"+data[index].text.replace(/(http:\/\/[a-zA-Z0-9_\.\-\/]*)/g,'<a href="$1" target="_blank">$1</a>').replace(/@([a-zA-Z0-9_\.\-\/]*)/g,'@<a href="http://twitter.com/$1" target="_blank">$1</a>').replace(/#([a-zA-Z0-9_\.\-\/]*)/g,'#<a href="http://twitter.com/#search?q=%23$1" target="_blank">$1</a>')+"</li>");
+				if (data[index].retweeted_status) {
+					img = data[index].retweeted_status.user.profile_image_url;
+				} else {
+					img = data[index].user.profile_image_url;
+				} 
+				$("section#twitter ul").append("<li><img src=\""+img+"\" />"+data[index].text.replace(/(http:\/\/[a-zA-Z0-9_\.\-\/]*)/g,'<a href="$1" target="_blank">$1</a>').replace(/@([a-zA-Z0-9_\.\-\/]*)/g,'@<a href="http://twitter.com/$1" target="_blank">$1</a>').replace(/#([a-zA-Z0-9_\.\-\/]*)/g,'#<a href="http://twitter.com/#search?q=%23$1" target="_blank">$1</a>')+"</li>");
 			}
 			$("section#twitter ul").fadeIn("slow");
 	});
@@ -15,7 +21,7 @@ $(document).ready(function(){
 			$("section#blog ul").hide();
 			if (data.length != 0) {
 				for (index in data) {
-					$("section#blog ul").append("<li><a href=\"" + data[index].slug + "\">" + data[index].title + "</a></li>");
+					$("section#blog ul").append("<li><a href=\"" + data[index].slug + "\">" + data[index].title + "</a> por " + data[index].author + "</li>");
 				}
 			} else {
 				$("section#blog ul").append("<li>Blog em andamento. Aguardem!</li>");
